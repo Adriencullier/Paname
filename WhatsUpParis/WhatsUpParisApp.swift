02f9b7985de9499c4429467e13cf955709da7aa7
Adriencullier <cullieradrien@gmail.com>
2023-11-23 09:14:40 +0100

    Initial Commit

---
+++ WhatsUpParis/WhatsUpParisApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpParisApp.swift
+//  WhatsUpParis
+//
+//  Created by Adrien Cullier on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpParisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
